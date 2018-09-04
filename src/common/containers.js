d8e79b3454b131c7577134d69978b4e46d8f7dc5
dev <13297470+tscr00@users.noreply.github.com>
2018-09-04 22:07:11 +0900

    Add coordinates to the edge container

--- src/common/containers.js
+++ src/common/containers.js
@@ -1,18 +1,20 @@
 
 class NodeContainer {
-  constructor(name, x, y, icon, label) {
+  constructor(name, icon, label, x, y) {
     this.name = name;
-    this.x = x;
-    this.y = y;
     this.icon = icon;
     this.label = label;
+    this.x = x;
+    this.y = y;
   }
 }
 
 class EdgeContainer {
-  constructor(from, to, label) {
+  constructor(from, to, label, x, y) {
     this.from = from;
     this.to = to;
     this.label = label;
+    this.x = x;
+    this.y = y;
   }
 }
